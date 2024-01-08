de8ee34f0a5b4adbe86767ff14398959ad37e930
DavideTarantino <davidetarantino2005@gmail.com>
2024-01-08 23:28:08 +0100

    inserimento della sezione di raccolta dati

---
+++ home.js
@@ -0,0 +1,16 @@
+// Variabili
+let 
+chilometri = document.getElementById ("km"), 
+eta = document.getElementById ("fasciaeta")
+
+//Costanti
+const prezzoalkm = 0.21;
+const scontominorenni = 20
+const scontoover = 40
+
+let km, fasciaeta;
+
+let bottonegenera = document.getElementById 
+("genera");
+
+let bottoneannulla = document.getElementById("annulla");
