f5fcdbe78475937cd3e68cf46225d1469307909a
DavideTarantino <davidetarantino2005@gmail.com>
2024-01-10 22:06:12 +0100

    correzione e fine esercizio

--- home.js
+++ home.js
@@ -1,16 +1,27 @@
 // Variabili
-let 
-chilometri = document.getElementById ("km"), 
-eta = document.getElementById ("fasciaeta")
 
-//Costanti
-const prezzoalkm = 0.21;
-const scontominorenni = 20
-const scontoover = 40
+let km = document.getElementById ("chilometri"); 
+let eta = document.getElementById ("eta");
+let x = Math.floor((Math.random() * 10) + 1);
+let y = Math.floor((Math.random() * 92994) + 1);
+let nomeoutput = document.getElementById("nome");
 
-let km, fasciaeta;
+function genera() {
+    document.getElementById("outputnome").innerHTML = nomeoutput.value
 
-let bottonegenera = document.getElementById 
-("genera");
-
-let bottoneannulla = document.getElementById("annulla");
+    if(eta.value < 18){
+        document.getElementById("output-offerta").innerHTML = "Sconto minorenni"
+        document.getElementById("output-prezzo").innerHTML = [((km.value * 0.21)*80)/100]
+    }
+    else if(eta.value >= 65){
+        document.getElementById("output-offerta").innerHTML = "Sconto per gli over 65"
+        document.getElementById("output-prezzo").innerHTML = [((km.value * 0.21)*60)/100]
+    }
+    else{
+        document.getElementById("output-offerta").innerHTML = "Biglietto standard"
+        document.getElementById("output-prezzo").innerHTML = (km.value * 0.21)
+    }
+   
+    document.getElementById("output-carrozza").innerHTML = x;
+    document.getElementById("output-cp").innerHTML = y;
+}
